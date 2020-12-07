e49660c167f480cc53a2e65c1ad1f34f1dcd7ae9
IA-Max <me@imaxyoung.com>
2020-12-07 11:35:47 +0800

    format

---
+++ content/blog/css-all-layout/index.md
@@ -0,0 +1,16 @@
+---
+title: CSS - 最全的布局
+date: '2020-02-12T09:29:34+08:00'
+status: publish
+permalink: /css-all-layout
+author: admin
+excerpt: ''
+type: post
+id: 1972
+category:
+    - React
+tag: []
+post_format: []
+---
+
+css layout
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,149 +1,149 @@
 module.exports = {
-  siteMetadata: {
-    title: `IMY Blog`,
-    author: {
-      name: `Max Young`,
-      summary: `who lives and works in GuangDong building useful things.`,
+    siteMetadata: {
+        title: `IMY Blog`,
+        author: {
+            name: `Max Young`,
+            summary: `who lives and works in GuangDong building useful things.`,
+        },
+        description: `A blog share what tech can do.`,
+        siteUrl: `https://imaxyoung.com`,
+        social: {
+            twitter: ``,
+        },
     },
-    description: `A blog share what tech can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: ``,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
+    plugins: [
+        {
+            resolve: `gatsby-transformer-remark`,
             options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (e.g. <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (e.g. for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
-              inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
-              aliases: {},
-              // This toggles the display of line numbers globally alongside the code.
-              // To use it, add the following line in gatsby-browser.js
-              // right after importing the prism color scheme:
-              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-              // Defaults to false.
-              // If you wish to only show line numbers on certain code blocks,
-              // leave false and use the {numberLines: true} syntax below
-              showLineNumbers: false,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
-              noInlineHighlight: false,
-              // This adds a new language definition to Prism or extend an already
-              // existing language definition. More details on this option can be
-              // found under the header "Add new language definition or extend an
-              // existing language" below.
-              languageExtensions: [
-                {
-                  language: "superscript",
-                  extend: "javascript",
-                  definition: {
-                    superscript_types: /(SuperType)/,
-                  },
-                  insertBefore: {
-                    function: {
-                      superscript_keywords: /(superif|superelse)/,
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            // Class prefix for <pre> tags containing syntax highlighting;
+                            // defaults to 'language-' (e.g. <pre class="language-js">).
+                            // If your site loads Prism into the browser at runtime,
+                            // (e.g. for use with libraries like react-live),
+                            // you may use this to prevent Prism from re-processing syntax.
+                            // This is an uncommon use-case though;
+                            // If you're unsure, it's best to use the default value.
+                            classPrefix: 'language-',
+                            // This is used to allow setting a language for inline code
+                            // (i.e. single backticks) by creating a separator.
+                            // This separator is a string and will do no white-space
+                            // stripping.
+                            // A suggested value for English speakers is the non-ascii
+                            // character '›'.
+                            inlineCodeMarker: null,
+                            // This lets you set up language aliases.  For example,
+                            // setting this to '{ sh: "bash" }' will let you use
+                            // the language "sh" which will highlight using the
+                            // bash highlighter.
+                            aliases: {},
+                            // This toggles the display of line numbers globally alongside the code.
+                            // To use it, add the following line in gatsby-browser.js
+                            // right after importing the prism color scheme:
+                            //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+                            // Defaults to false.
+                            // If you wish to only show line numbers on certain code blocks,
+                            // leave false and use the {numberLines: true} syntax below
+                            showLineNumbers: false,
+                            // If setting this to true, the parser won't handle and highlight inline
+                            // code used in markdown i.e. single backtick code like `this`.
+                            noInlineHighlight: false,
+                            // This adds a new language definition to Prism or extend an already
+                            // existing language definition. More details on this option can be
+                            // found under the header "Add new language definition or extend an
+                            // existing language" below.
+                            languageExtensions: [
+                                {
+                                    language: 'superscript',
+                                    extend: 'javascript',
+                                    definition: {
+                                        superscript_types: /(SuperType)/,
+                                    },
+                                    insertBefore: {
+                                        function: {
+                                            superscript_keywords: /(superif|superelse)/,
+                                        },
+                                    },
+                                },
+                            ],
+                            // Customize the prompt used in shell output
+                            // Values below are default
+                            prompt: {
+                                user: 'root',
+                                host: 'localhost',
+                                global: false,
+                            },
+                            // By default the HTML entities <>&'" are escaped.
+                            // Add additional HTML escapes by providing a mapping
+                            // of HTML entities and their escape value IE: { '}': '&#123;' }
+                            escapeEntities: {},
+                        },
                     },
-                  },
-                },
-              ],
-              // Customize the prompt used in shell output
-              // Values below are default
-              prompt: {
-                user: "root",
-                host: "localhost",
-                global: false,
-              },
-              // By default the HTML entities <>&'" are escaped.
-              // Add additional HTML escapes by providing a mapping
-              // of HTML entities and their escape value IE: { '}': '&#123;' }
-              escapeEntities: {},
+                ],
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 630,
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
             },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 630,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            },
+        },
+        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `content/assets/gatsby-icon.png`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};
